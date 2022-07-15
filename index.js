3137835d366216e95ecf164b39a82abab9dbc140
yangchaohao <yangchaohao@taou.com>
2022-07-15 21:34:07 +0800

    feat:test

--- index.js
+++ index.js
@@ -18,21 +18,30 @@ try {
     jdPtKey: !!jdPtKey,
   });
 
+  console.log("参数情况====>", {
+    qqEmail,
+    qqEmailPass,
+    juejinCookie,
+    jdPtPin,
+    jdPtKey,
+  });
+
   Promise.all([
     juejinJoinIn({ juejinCookie }),
     jinDouJoinIn({ jdPtPin, jdPtKey }),
   ])
     .then(([juejinResult, jinDouResult]) => {
-      let content = "";
-      let res = "";
-      if (juejinResult) {
-        content += `掘金签到&抽奖结果：<br/>${juejinResult.content}`;
-        res += `掘金签到&抽奖接口返回：<br/>${juejinResult.result}`;
-      }
-      if (jinDouResult) {
-        content += `京豆签到结果：<br/>${jinDouResult.content}`;
-        res += `京豆接口返回：<br/>${jinDouResult.result}`;
-      }
+      // let content = "";
+      // let res = "";
+      // if (juejinResult) {
+      //   content += `掘金签到&抽奖结果：<br/>${juejinResult.content}`;
+      //   res += `掘金签到&抽奖接口返回：<br/>${juejinResult.result}`;
+      // }
+      // if (jinDouResult) {
+      //   content += `京豆签到结果：<br/>${jinDouResult.content}`;
+      //   res += `京豆接口返回：<br/>${jinDouResult.result}`;
+      // }
+
       sendQQEmail({
         subject: "Github Actions签到完毕，靓仔",
         html: `${content}${res}`,
